e80f30e04f7ce9a7f510937ffdb52004057f5e14
GuiSaiUwU <107002177+GuiSaiUwU@users.noreply.github.com>
2026-08-17 17:26:23 -0300

    feat: simplificado App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import TelaPrincipal from './src/TelaPrincipal';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <TelaPrincipal/>
-    </View>
-  );
+  return <TelaPrincipal />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
